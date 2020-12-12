2674626d30e7441097ba7bce4f87fc29fbff9b7a
Sebastian Ronquist Vågström <seron94@gmail.com>
2020-12-12 17:53:59 +0900

    Solved the store bs. And added a logger for redux when actions are dispatched

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -5,6 +5,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import ReduxThunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+function logger({ getState }) {
+  return (next) => (action) => {
+    console.log("will dispatch", action);
+    const returnValue = next(action);
+    console.log("state after dispatch", getState());
+
+    return returnValue;
+  };
+}
+
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
@@ -18,7 +28,8 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   persistedReducer,
-  composeEnhancer(applyMiddleware(ReduxThunk))
+  composeEnhancer(applyMiddleware(ReduxThunk, logger))
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 let persistor = persistStore(store);
