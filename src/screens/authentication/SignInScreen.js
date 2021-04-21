c4fe95038e4160e3b30228a2c380cf718adc173c
Sebastian Ronquist Vågström <seron94@gmail.com>
2021-04-21 02:01:08 +0200

    Refactored SignInScreen and disabled autocorrect on email and password input.

--- src/screens/authentication/SignInScreen.js
+++ src/screens/authentication/SignInScreen.js
@@ -1,102 +1,103 @@
 import React, {useEffect} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text, Button, ActivityIndicator} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import {TextInput} from 'react-native-gesture-handler';
 import {connect} from 'react-redux';
-import { updateEmail, updatePassword, login, getUser, signedIn } from "../../redux/ActionCreators/UserActions";
+import {updateEmail, updatePassword, login, getUser, signedIn} from "../../redux/ActionCreators/UserActions";
 
 const SignInScreen = (props) => {
 
-  // useEffect(() => {
-  //   Firebase.auth().onAuthStateChanged(user => {
-  //     if (user) {
-  //       props.getUser(user.uid)
-  //       if(props.user != null){
-  //         props.signedIn(true);
-  //       }
-  //     }else {
-        
-  //     }
-  //   })
-  // });
+    // useEffect(() => {
+    //   Firebase.auth().onAuthStateChanged(user => {
+    //     if (user) {
+    //       props.getUser(user.uid)
+    //       if(props.user != null){
+    //         props.signedIn(true);
+    //       }
+    //     }else {
 
-  if(props.loading){
+    //     }
+    //   })
+    // });
+
+    if (props.loading) {
+        return (
+            <View style={styles.activityIndicator}>
+                <ActivityIndicator size="large"/>
+            </View>
+        );
+    }
+
     return (
-      <View style={styles.activityIndicator}>
-        <ActivityIndicator size="large"/>
-      </View>
-      );
-  }
-
-  return(
-    <View style={styles.container}>
-      <TextInput style={styles.inputBox} 
-        placeholder="Email"
-        onChangeText={email => props.updateEmail(email)}
-         />
-      <TextInput style={styles.inputBox}
-        placeholder="Password"
-        onChangeText={password => props.updatePassword(password)}
-        secureTextEntry={true}/>
-      <TouchableOpacity style={styles.button} onPress={() => props.login(props.email, props.password)}>
-          <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <Button title="Don't have an account yet? Sign up" onPress={() => props.navigation.navigate("SignUp")}/>
-    </View>
-  );
+        <View style={styles.container}>
+            <TextInput style={styles.inputBox}
+                       autoCorrect={false}
+                       placeholder="Email"
+                       onChangeText={email => props.updateEmail(email)}
+            />
+            <TextInput style={styles.inputBox}
+                       autoCorrect={false}
+                       placeholder="Password"
+                       onChangeText={password => props.updatePassword(password)}
+                       secureTextEntry={true}/>
+            <TouchableOpacity style={styles.button} onPress={() => props.login(props.email, props.password)}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <Button title="Don't have an account yet? Sign up" onPress={() => props.navigation.navigate("SignUp")}/>
+        </View>
+    );
 }
 
 const mapStateToProps = (state) => {
-  return {
-    email: state.user.email,
-    password: state.user.password,
-    loading: state.user.loading,
-  }
+    return {
+        email: state.user.email,
+        password: state.user.password,
+        loading: state.user.loading,
+    }
 }
 
 const mapDispatchToProps = {
-  updateEmail, updatePassword, login, getUser, signedIn
+    updateEmail, updatePassword, login, getUser, signedIn
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(SignInScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(SignInScreen);
 
-const styles= StyleSheet.create({
-  activityIndicator: {
-    flex: 1,
-    justifyContent: 'center', 
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputBox: {
-    width: "60%",
-    margin: 10,
-    padding: 15,
-    fontSize: 16,
-    borderColor: 'black',
-    borderBottomWidth: 1,
-  },
-  button: {
-    marginTop: 30,
-    marginBottom: 20,
-    paddingVertical: 5,
-    alignItems: 'center',
-    backgroundColor: '#F6820D',
-    borderColor: '#F6820D',
-    borderWidth: 1,
-    borderRadius: 5,
-    width: 200
-},
-  buttonText: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: '#fff'
-  },
-  buttonSignup: {
-      fontSize: 12
-  }
+const styles = StyleSheet.create({
+    activityIndicator: {
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        padding: 10,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputBox: {
+        width: "60%",
+        margin: 10,
+        padding: 15,
+        fontSize: 16,
+        borderColor: 'black',
+        borderBottomWidth: 1,
+    },
+    button: {
+        marginTop: 30,
+        marginBottom: 20,
+        paddingVertical: 5,
+        alignItems: 'center',
+        backgroundColor: '#F6820D',
+        borderColor: '#F6820D',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 200
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    buttonSignup: {
+        fontSize: 12
+    }
 })
